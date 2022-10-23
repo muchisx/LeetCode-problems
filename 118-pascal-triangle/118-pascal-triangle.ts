7341aec06604d343212d18bc8638fb3d6bfc4a48
muchisx <mnm.soul@gmail.com>
2022-10-23 00:08:48 -0500

    Pascal Triangle init

---
+++ 118-pascal-triangle/118-pascal-triangle.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/pascals-triangle/
+
+
+const generate = (numRows: number): number[][] => {
+  
+  const pascalTree: number[][] = [];
+
+  if (1 <= numRows && numRows <= 30) {
+
+    for (let i: number = 0; i < numRows; i++) {
+      
+      const ii: number = i + 1;
+
+      pascalTree.push([1])
+
+    }
+  }
+
+  
+
+  return pascalTree
+}
+
+generate(3);
